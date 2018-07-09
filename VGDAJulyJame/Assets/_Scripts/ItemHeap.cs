edac18b49acc2781b27b542e1423e674adbec425
Eric <eric.lee0051@gmail.com>
2018-07-08 17:17:05 -0700

    A* implementation

---
+++ VGDAJulyJame/Assets/_Scripts/ItemHeap.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemHeap<T> where T : IHeapItem<T>
+{
+    T[] items;
+    int currentItemCount;
+
+    public ItemHeap(int maxHeapSize)
+    {
+        items = new T[maxHeapSize];
+    }
+
+    public void Swap(T itemA, T itemB)
+    {
+        // swap item location
+        items[itemA.HeapIndex] = itemB;
+        items[itemB.HeapIndex] = itemA;
+
+        // update swaped index numbers
+        int itemAIndex = itemA.HeapIndex;
+        itemA.HeapIndex = itemB.HeapIndex;
+        itemB.HeapIndex = itemAIndex;
+    }
+
+    public void PercolateUp(T item)
+    {
+        int parentIndex = (item.HeapIndex - 1) / 2;
+        // compare specified item with values of parent
+        while(true)
+        {
+            T parentItem = items[parentIndex];
+
+            if (item.CompareTo(parentItem) > 0)
+                Swap(item, parentItem);
+            else
+                break;
+
+            // compare to next parent (traverses up tree)
+            parentIndex = (item.HeapIndex - 1) / 2;
+        }
+    }
+
+    // insert item at top of heap, swap with children if item is larger than child node
+    public void PercolateDown(T item)
+    {
+        while (true)
+        {
+            int leftChildIndex = 2 * item.HeapIndex + 1;
+            int rightChildIndex = 2 * item.HeapIndex + 2;
+
+            int swapIndex = 0;
+
+            if (leftChildIndex < currentItemCount)
+            {
+                swapIndex = leftChildIndex;
+
+                if (rightChildIndex < currentItemCount)
+                {
+                    if (items[leftChildIndex].CompareTo(items[rightChildIndex]) < 0)
+                    {
+                        swapIndex = rightChildIndex;
+                    }
+                }
+
+                if (item.CompareTo(items[swapIndex]) < 0)
+                {
+                    Swap(item, items[swapIndex]);
+                }
+                else
+                    return;
+            }
+            else
+                return;
+        }
+    }
+
+    // add item into heap
+    public void Add(T item)
+    {
+        item.HeapIndex = currentItemCount;
+        items[currentItemCount] = item;
+
+        PercolateUp(item);
+        currentItemCount++;
+    }
+
+    // return value of top of heap, removes top of heap
+    public T Pop()
+    {
+        T firstItem = items[0];
+
+        currentItemCount--;
+
+        items[0] = items[currentItemCount];
+        items[0].HeapIndex = 0;
+        PercolateDown(items[0]);
+        
+        return firstItem;
+    }
+
+    // check if item is within heap
+    public bool Contains(T item)
+    {
+        return Equals(items[item.HeapIndex], item);
+    }
+
+    // change priority of an item
+    public void UpdateItem(T item)
+    {
+        PercolateUp(item);
+    }
+
+    public int Count { get { return currentItemCount; } }
+}
+
+public interface IHeapItem<T> : IComparable<T>
+{
+    int HeapIndex { get; set; }
+}
---
+++ VGDAJulyJame/Assets/_Scripts/Pathfind.cs
@@ -0,0 +1,112 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pathfind : MonoBehaviour
+{
+    // temp, represents object trying to reach other object
+    public Transform seeker, target;
+
+    MapGrid grid;
+
+    void Awake()
+    {
+        grid = GetComponent<MapGrid>();
+    }
+
+    // temp, updates path from seeker to target on update
+    void Update()
+    {
+        FindPath(seeker.position, target.position);
+    }
+
+    // finds a path within the grid from start node to end node
+    void FindPath(Vector2 startPos, Vector2 endPos)
+    {
+        ItemHeap<Node> openSet = new ItemHeap<Node>(grid.MaxSize);
+        HashSet<Node> closedSet = new HashSet<Node>();
+
+        Node startNode = grid.WorldToNodePoint(startPos);
+        Node targetNode = grid.WorldToNodePoint(endPos);
+
+        openSet.Add(startNode);
+        print(openSet.Count);
+        // while nodes that have not been explored exist
+        while (openSet.Count > 0)
+        {
+            // retrieves first item from heap via pop
+            Node currentNode = openSet.Pop();
+            
+            closedSet.Add(currentNode);
+
+            // if the target node has been found, loop ends and call to follow the found path
+            if (currentNode == targetNode)
+            {
+                //print("target found");
+                //print("start location = " + startPos.x + "," + startPos.y);
+                //print("target location = " + targetNode.gridX + "," + targetNode.gridY);
+
+                FollowPath(startNode, targetNode);
+                return;
+            }
+
+            // for each neighboring node near by current node
+            foreach (Node n in grid.GetNeighbourNodes(currentNode))
+            {
+                if (!n.passable || closedSet.Contains(n))
+                {
+                    continue;
+                }
+
+                int distanceFromNeighbour = currentNode.gCost + GetDistance(currentNode, n);
+
+                if (distanceFromNeighbour < n.gCost || !openSet.Contains(n))
+                {
+                    n.gCost = distanceFromNeighbour;
+                    n.hCost = GetDistance(n, targetNode);
+
+                    n.parentNode = currentNode;
+
+                    if(!openSet.Contains(n))
+                    {
+                        openSet.Add(n);
+                    }
+
+                    openSet.UpdateItem(n);
+                }
+            }
+        }
+    }
+
+    // find the distance between 2 nodes
+    int GetDistance(Node x, Node y)
+    {
+        int xDistance = Mathf.Abs(y.gridX - x.gridX);
+        int yDistance = Mathf.Abs(y.gridY - x.gridY);
+
+        int totalDistance;
+        if (xDistance > yDistance)
+            totalDistance = 14 * yDistance + 10 * (xDistance - yDistance);
+        else
+            totalDistance = 14 * xDistance + 10 * (yDistance - xDistance);
+
+        return totalDistance;
+    }
+
+    // follows path of nodes from start node to end node, using set parent node of the end node
+    void FollowPath(Node start, Node end)
+    {
+        List<Node> path = new List<Node>();
+        Node currentNode = end;
+
+        while(currentNode != start)
+        {
+            path.Add(currentNode);
+
+            currentNode = currentNode.parentNode;
+        }
+
+        path.Reverse();
+        grid.path = path;
+    }
+}
