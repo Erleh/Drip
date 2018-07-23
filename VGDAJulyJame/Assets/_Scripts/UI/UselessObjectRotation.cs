7f2d1587c853ad4e1c3da869243465c9d35c3ec0
Paper-Hat <justinkristian@gmail.com>
2018-07-23 02:56:56 -0700

    UI Polish

---
+++ VGDAJulyJame/Assets/_Scripts/UI/UselessObjectRotation.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UselessObjectRotation : MonoBehaviour {
+
+    [SerializeField]
+    private float maxTimer;
+    private float rotationSpeed;
+    public void RotateObject(float rotSpd)
+    {
+        rotationSpeed = rotSpd;
+        StartCoroutine(RotateObjectCo());
+    }
+
+    IEnumerator RotateObjectCo()
+    {
+        float timer = 0f;
+        while (timer <= maxTimer)
+        {
+            transform.Rotate(new Vector3(0f, 0f, rotationSpeed * Time.deltaTime * 360f));
+            timer += Time.deltaTime;
+            yield return null;
+        }
+    }
+}
