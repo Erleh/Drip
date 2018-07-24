30065cd03a75dd9098cc8cea0a4f3ff88abc5820
Daniel Kim <dankimaudio@gmail.com>
2018-07-24 00:40:47 -0700

    Sounds Finally in!

---
+++ VGDAJulyJame/Assets/AdjustmentButton.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AdjustmentButton : MonoBehaviour {
+
+    public void onRelease()
+    {
+        AkSoundEngine.PostEvent("Release_Adjustment", gameObject);
+    }
+
+    public void onMouseOver()
+    {
+        AkSoundEngine.PostEvent("MouseOver", gameObject);
+    }
+    public void onClick()
+    {
+        AkSoundEngine.PostEvent("MouseOver", gameObject);
+    }
+}
---
+++ VGDAJulyJame/Assets/MouseOverButton.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseOverButton : MonoBehaviour {
+
+	public void onMouseOver()
+    {
+        AkSoundEngine.PostEvent("MouseOver", gameObject);
+    }
+
+}
