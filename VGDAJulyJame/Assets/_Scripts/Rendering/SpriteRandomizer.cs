aabfc64eb1afa1d9a1ab530e2936120b25445eec
Paper-Hat <justinkristian@gmail.com>
2018-07-26 18:35:22 -0700

    Prefab Fixes, error correction, and sprite randomizer

---
+++ VGDAJulyJame/Assets/_Scripts/Rendering/SpriteRandomizer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteRandomizer : MonoBehaviour {
+
+
+    [SerializeField]
+    private List<Sprite> sprites;
+    private SpriteRenderer sr;
+
+    public void Awake()
+    {
+        Init();
+    }
+    private void Init()
+    {
+        if(sprites.Count > 0)
+            sr = GetComponentInChildren<SpriteRenderer>();
+    }
+    //Grabs random sprite from entire list when it is turned on
+    public void OnEnable()
+    {
+        sr.sprite = sprites[Random.Range(0, sprites.Count)];
+    }
+}
