4f91918962ccd718a63801ebbc1b50d6ce0459de
Paper-Hat <justinkristian@gmail.com>
2018-07-27 17:31:42 -0700

    GameOverFade

--- VGDAJulyJame/Assets/_Scripts/UI/GameOverFade.cs
+++ VGDAJulyJame/Assets/_Scripts/UI/GameOverFade.cs
@@ -1,12 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameOverFade : MonoBehaviour {
 
-    
+    [SerializeField]
+    private Image deathScreen;
+    [SerializeField]
+    private Button[] deathScreenUIButtons;
+    [SerializeField]
+    private float fadeDuration;
+
     public void FadeToDeathScreen()
     {
-
+        foreach (Button b in deathScreenUIButtons)
+            StartCoroutine(FadeInImg(b.image, fadeDuration));
+        StartCoroutine(FadeInImg(deathScreen, fadeDuration));
+    }
+    IEnumerator FadeInImg(Image img, float duration)
+    {
+        img.CrossFadeAlpha(255f, duration, true);
+        yield return null;
     }
 }
