7aed7896fdb39d30b4c5fbdd2a516c09b677133d
Paper-Hat <justinkristian@gmail.com>
2018-07-27 19:53:53 -0700

    UnityEvents

--- VGDAJulyJame/Assets/_Scripts/Player/PlayerState.cs
+++ VGDAJulyJame/Assets/_Scripts/Player/PlayerState.cs
@@ -11,8 +11,8 @@ public class PlayerState : MonoBehaviour
     private PlayerMovement pMovement;
     bool _isAlive;
 
-    public delegate void onDeathEvent();
-    public event onDeathEvent Died;
+    //public delegate void onDeathEvent();
+    //public event onDeathEvent Died;
 
     void Awake()
     {
@@ -23,14 +23,19 @@ public class PlayerState : MonoBehaviour
     public bool IsAlive(){  return _isAlive;                }
     public bool Moving(){   return pMovement.GetMoving();   }
     public bool Pushing(){  return pMovement.GetPushing();  }
+    public void OnAttacked()
+    {
+        Debug.Log("Player Was Attacked.");
+        onDeath.Invoke();
+        Debug.Log("Called onDeath functions.");
+    }
     public void Die()
     {
         _isAlive = false;
         GetComponent<Animator>().SetTrigger("die");
-        AkSoundEngine.PostEvent("Player_Death");
-        Died();
+        //AkSoundEngine.PostEvent("Player_Death");
+        Debug.Log("Triggered player death.");
     }
-
     private void Update()
     {
         if (IsAlive()){AkSoundEngine.PostEvent("Player_Idle");}
--- VGDAJulyJame/Assets/_Scripts/UI/GameOverFade.cs
+++ VGDAJulyJame/Assets/_Scripts/UI/GameOverFade.cs
@@ -11,16 +11,22 @@ public class GameOverFade : MonoBehaviour {
     private Button[] deathScreenUIButtons;
     [SerializeField]
     private float fadeDuration;
-
+    [SerializeField]
+    private float delayDuration;
     public void FadeToDeathScreen()
     {
+        StartCoroutine(Delay(fadeDuration));
         foreach (Button b in deathScreenUIButtons)
             StartCoroutine(FadeInImg(b.image, fadeDuration));
         StartCoroutine(FadeInImg(deathScreen, fadeDuration));
     }
+    IEnumerator Delay(float duration)
+    {
+        yield return new WaitForSeconds(duration);
+    }
     IEnumerator FadeInImg(Image img, float duration)
     {
-        img.CrossFadeAlpha(255f, duration, true);
+        img.CrossFadeAlpha(1, duration, true);
         yield return null;
     }
 }
