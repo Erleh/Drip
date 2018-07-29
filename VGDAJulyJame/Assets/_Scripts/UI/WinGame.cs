a42375956ca6a94622c733d3f6fb20cf12459ea6
Paper-Hat <justinkristian@gmail.com>
2018-07-28 22:00:15 -0700

    game winning trigger

---
+++ VGDAJulyJame/Assets/_Scripts/UI/WinGame.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+public class WinGame : MonoBehaviour {
+
+    [SerializeField]
+    private UnityEvent winGame;
+	
+	// Update is called once per frame
+    void OnTriggerEnter2D(Collider2D col) {
+        winGame.Invoke();
+	}
+}
