429aa56f55ff4d34a6b3cf3ff5daad05bc838915
Paper-Hat <justinkristian@gmail.com>
2018-07-28 21:25:29 -0700

    UI

---
+++ VGDAJulyJame/Assets/_Scripts/UI/PauseHandler.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseHandler : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject PauseMenu;
+
+    private bool paused;
+
+	void Update ()
+    {
+        if (Input.GetKeyDown("Pause"))
+            HandlePause();
+	}
+    public void HandlePause()
+    {
+        paused = !paused;
+        if (!paused)
+        {
+            PauseMenu.SetActive(true);
+            Time.timeScale = 0;
+        }
+        else
+        {
+            PauseMenu.SetActive(false);
+            Time.timeScale = 1;
+        }
+    }
+}
