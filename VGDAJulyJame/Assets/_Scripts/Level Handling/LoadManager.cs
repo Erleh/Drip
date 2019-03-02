52dc0bcccc5edb3363ebe920fdb7b4b3b72210f1
Paper-Hat <justinkristian@gmail.com>
2019-03-02 00:09:54 -0800

    new fade

---
+++ VGDAJulyJame/Assets/_Scripts/Level Handling/LoadManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+
+public static class LoadManager
+{
+    
+    static IEnumerator AsyncLoadCo(string level, Image loadingImage, float duration)
+    {
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(level);
+        while (!asyncLoad.isDone)
+        {
+            loadingImage.canvasRenderer.SetAlpha(0.0f);
+            loadingImage.CrossFadeAlpha(255f, duration, false);
+            yield return null;
+        }
+    }
+
+    static IEnumerator BeginLevelCo()
+    {
+        
+    }
+}
