96eb833c899daad041bdac9640b602a17cf5ae80
Paper-Hat <justinkristian@gmail.com>
2019-03-05 17:34:25 -0800

    Fades working properly

--- VGDAJulyJame/Assets/_Scripts/Level Handling/LoadManager.cs
+++ VGDAJulyJame/Assets/_Scripts/Level Handling/LoadManager.cs
@@ -1,26 +1,43 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Experimental.Rendering.LightweightPipeline;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 
 public static class LoadManager
 {
-    
-    static IEnumerator AsyncLoadCo(string level, Image loadingImage, float duration)
+    public static Coroutine startLevel, endLevel;
+    private static float tolerance = 1f, maxAlpha = 255f, minAlpha = 0f;
+    public static IEnumerator AsyncLoadCo(string level, Image loadingImage, float duration)
     {
+        Time.timeScale = 0;
         AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(level);
-        while (!asyncLoad.isDone)
-        {
-            loadingImage.canvasRenderer.SetAlpha(0.0f);
-            loadingImage.CrossFadeAlpha(255f, duration, false);
+        asyncLoad.allowSceneActivation = false;
+        
+        loadingImage.canvasRenderer.SetAlpha(0f);
+        while (!asyncLoad.isDone && loadingImage.canvasRenderer.GetAlpha() < maxAlpha - tolerance){
+            loadingImage.CrossFadeAlpha(255f, duration, true);
             yield return null;
         }
+
+        asyncLoad.allowSceneActivation = true;
+        endLevel = null;
+        Time.timeScale = 1;
     }
 
-    /*static IEnumerator BeginLevelCo()
+    public static IEnumerator BeginLevelCo(Image loadingImage, float duration, float delay)
     {
-        
-    }*/
+        Time.timeScale = 0;
+        loadingImage.canvasRenderer.SetAlpha(255f);
+        while (loadingImage.canvasRenderer.GetAlpha() > minAlpha + tolerance)
+        {
+            loadingImage.CrossFadeAlpha(0, duration, true);
+            yield return null;
+        }
+        yield return new WaitForSecondsRealtime(delay);
+        startLevel = null;
+        Time.timeScale = 1;
+    }
 }
---
+++ VGDAJulyJame/Assets/_Scripts/UI/LevelHandler.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelHandler : MonoBehaviour
+{
+    public Image fadeOutImage, fadeInImage;
+    public float fadeToGameSeconds, fadeToLoadSeconds, delayBeforeInput;
+    public string levelToLoad;
+    void Start()
+    {
+        DontDestroyOnLoad(this);
+        LoadManager.startLevel = StartCoroutine(LoadManager.BeginLevelCo(fadeOutImage, fadeToGameSeconds, delayBeforeInput));
+    }
+
+    public void LoadNextLevel()
+    {
+        LoadManager.endLevel = StartCoroutine(LoadManager.AsyncLoadCo(levelToLoad, fadeInImage, fadeToLoadSeconds));
+    }
+}
